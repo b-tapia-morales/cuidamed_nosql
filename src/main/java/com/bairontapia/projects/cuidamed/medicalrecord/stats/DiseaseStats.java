1b9cea9e58c5619d39d1b5dee2b90270ca7c5bf3
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-18 23:59:55 -0400

    Eliminated all unused classes in project

--- src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/stats/DiseaseStats.java
+++
@@ -1,8 +0,0 @@
-package com.bairontapia.projects.cuidamed.medicalrecord.stats;
-
-public record DiseaseStats(String diseaseName, Integer frequency) {
-
-  public static DiseaseStats createInstance(String diseaseName, Integer frequency) {
-    return new DiseaseStats(diseaseName, frequency);
-  }
-}
--- src/main/java/com/bairontapia/projects/cuidamed/medicalrecord/stats/MedicationStats.java
+++
@@ -1,9 +0,0 @@
-package com.bairontapia.projects.cuidamed.medicalrecord.stats;
-
-public record MedicationStats(String name, Integer frequency) {
-
-  public static MedicationStats createInstance(String name, Integer frequency) {
-    return new MedicationStats(name, frequency);
-  }
-
-}
