41e15714967f9e46e3f2ca39ec6453b7ec7dfc42
Benjamín López <benjamin.lopezl@userena.cl>
2022-05-19 23:19:38 -0400

    Created ElderPOJODAO and MongoConnection Singleton.

--- src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
+++ src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
@@ -4,7 +4,9 @@ import static com.mongodb.client.model.Filters.eq;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
+import com.bairontapia.projects.cuidamed.connection.MongoConnectionSingleton;
 import com.bairontapia.projects.cuidamed.pojo.ElderPOJO;
+import com.bairontapia.projects.cuidamed.pojo.ElderPOJODAO;
 import com.bairontapia.projects.cuidamed.pojo.RoutineCheckupPOJO;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
@@ -17,79 +19,80 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 
 public class CuidaMedApplication {
 
-  public static void main(String... args) throws SQLException, IOException {
+  public static void main(String... args) {
     var mongoLogger = Logger.getLogger("org.mongodb.driver");
     mongoLogger.setLevel(Level.WARNING);
-    var pojoCodecRegistry =
-        fromRegistries(
-            MongoClient.getDefaultCodecRegistry(),
-            fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-    try (var mongoClient =
-        new MongoClient(
-            "localhost:27017",
-            MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build())) {
 
-      // cambiar el nombre de la base de datos a admin
-      var database = mongoClient.getDatabase("Cuidamed_DB");
-      var elderColl = database.getCollection("elder", ElderPOJO.class);
-      var routineCheckupColl = database.getCollection("routine_checkup", RoutineCheckupPOJO.class);
-      var routineCheckups = new ArrayList<RoutineCheckupPOJO>();
-      var e = elderColl.find(eq("rut", "3988832-7")).first();
-      var rc = routineCheckupColl.find(eq("elderId", e.getId())).into(routineCheckups);
-      System.out.println(e);
-      System.out.println(e.getResponsible().toString());
-      System.out.println(routineCheckups);
-      /*
-      var database = mongoClient.getDatabase("admin");
-      var elderColl = database.getCollection("elder", ElderPOJO.class);
-      elderColl.drop();
-      var routineCheckupColl = database.getCollection("routine_checkup", RoutineCheckupPOJO.class);
-      routineCheckupColl.drop();
-      var administrationColl = database.getCollection("medication_administration",
-          MedicationAdministrationPOJO.class);
-      administrationColl.drop();
-      for (var elder : ElderDAO.getInstance().findAll()) {
-        var elderId = new ObjectId();
-        var diagnostics = DiagnosticDAO.getInstance().findAll(elder.rut());
-        var diagnosticPOJOS = new ArrayList<DiagnosticPOJO>();
-        for (var diagnostic : diagnostics) {
-          var disease = DiseaseDAO.getInstance().find(diagnostic.diseaseName()).orElseThrow();
-          var prescriptions =
-              MedicationPrescriptionDAO.getInstance().findByRutAndDiseaseName(elder.rut(),
-                  disease.name());
-          var prescriptionPOJOS = new ArrayList<MedicationPrescriptionPOJO>();
-          for (var prescription : prescriptions) {
-            var medication =
-                MedicationDAO.getInstance().find(prescription.medicationName()).orElseThrow();
-            var administrations =
-                MedicationAdministrationDAO.getInstance().findByRutAndMedicationName(elder.rut(),
-                    medication.name());
-            var administrationPOJOS = administrations.stream()
-                .map(MedicationAdministrationPOJO::new).toList();
-            administrationColl.insertMany(administrationPOJOS);
-            var administrationIds =
-                administrationPOJOS.stream().map(MedicationAdministrationPOJO::getId).toList();
-            prescriptionPOJOS.add(
-                new MedicationPrescriptionPOJO(prescription, medication, administrationIds));
-          }
-          diagnosticPOJOS.add(new DiagnosticPOJO(diagnostic, disease, prescriptionPOJOS));
+    var mongoConnection = MongoConnectionSingleton.getConnection();
+
+    //var elderColl = database.getCollection("elder", ElderPOJO.class);
+    //var routineCheckupColl = database.getCollection("routine_checkup", RoutineCheckupPOJO.class);
+    var routineCheckups = new ArrayList<RoutineCheckupPOJO>();
+    var elderDao = new ElderPOJODAO();
+    var e = elderDao.find("3988832-7");
+    e.orElseThrow().setFirstName("el pepe");
+    elderDao.update(e.get());
+    System.out.println(elderDao.find("3988832-7"));
+    //System.out.println(elderDao.findAll());
+    /*
+    var e = elderColl.find(eq("rut", "3988832-7")).first();
+    var rc = routineCheckupColl.find(eq("elderId", e.getId())).into(routineCheckups);
+    System.out.println(e);
+    System.out.println(e.getResponsible().toString());
+     */
+
+    //System.out.println(routineCheckups);
+    /*
+    var database = mongoClient.getDatabase("admin");
+    var elderColl = database.getCollection("elder", ElderPOJO.class);
+    elderColl.drop();
+    var routineCheckupColl = database.getCollection("routine_checkup", RoutineCheckupPOJO.class);
+    routineCheckupColl.drop();
+    var administrationColl = database.getCollection("medication_administration",
+        MedicationAdministrationPOJO.class);
+    administrationColl.drop();
+    for (var elder : ElderDAO.getInstance().findAll()) {
+      var elderId = new ObjectId();
+      var diagnostics = DiagnosticDAO.getInstance().findAll(elder.rut());
+      var diagnosticPOJOS = new ArrayList<DiagnosticPOJO>();
+      for (var diagnostic : diagnostics) {
+        var disease = DiseaseDAO.getInstance().find(diagnostic.diseaseName()).orElseThrow();
+        var prescriptions =
+            MedicationPrescriptionDAO.getInstance().findByRutAndDiseaseName(elder.rut(),
+                disease.name());
+        var prescriptionPOJOS = new ArrayList<MedicationPrescriptionPOJO>();
+        for (var prescription : prescriptions) {
+          var medication =
+              MedicationDAO.getInstance().find(prescription.medicationName()).orElseThrow();
+          var administrations =
+              MedicationAdministrationDAO.getInstance().findByRutAndMedicationName(elder.rut(),
+                  medication.name());
+          var administrationPOJOS = administrations.stream()
+              .map(MedicationAdministrationPOJO::new).toList();
+          administrationColl.insertMany(administrationPOJOS);
+          var administrationIds =
+              administrationPOJOS.stream().map(MedicationAdministrationPOJO::getId).toList();
+          prescriptionPOJOS.add(
+              new MedicationPrescriptionPOJO(prescription, medication, administrationIds));
         }
-        var responsible = ResponsibleDAO.getInstance().find(elder.responsibleRut()).orElseThrow();
-        var responsiblePOJO = new ResponsiblePOJO(responsible);
-        var medicalRecord = MedicalRecordDAO.getInstance().find(elder.rut()).orElseThrow();
-        var medicalRecordPOJO = new MedicalRecordPOJO(medicalRecord);
-        var elderPOJO = new ElderPOJO(elderId, elder, responsiblePOJO, medicalRecordPOJO,
-            diagnosticPOJOS);
-        elderColl.insertOne(elderPOJO);
-        var routineCheckupPOJOS =
-            RoutineCheckupDAO.getInstance().findAll(medicalRecord.rut()).stream()
-                .map(e -> new RoutineCheckupPOJO(e, elderId)).toList();
-        routineCheckupColl.insertMany(routineCheckupPOJOS);
-        routineCheckupColl.createIndex(Indexes.hashed("elderId"));
-        elderColl.createIndex(Indexes.hashed("{diagnostics.medicationPrescriptions"
-            + ".administrationIds:1}"));
+        diagnosticPOJOS.add(new DiagnosticPOJO(diagnostic, disease, prescriptionPOJOS));
       }
-       */
+      var responsible = ResponsibleDAO.getInstance().find(elder.responsibleRut()).orElseThrow();
+      var responsiblePOJO = new ResponsiblePOJO(responsible);
+      var medicalRecord = MedicalRecordDAO.getInstance().find(elder.rut()).orElseThrow();
+      var medicalRecordPOJO = new MedicalRecordPOJO(medicalRecord);
+      var elderPOJO = new ElderPOJO(elderId, elder, responsiblePOJO, medicalRecordPOJO,
+          diagnosticPOJOS);
+      elderColl.insertOne(elderPOJO);
+      var routineCheckupPOJOS =
+          RoutineCheckupDAO.getInstance().findAll(medicalRecord.rut()).stream()
+              .map(e -> new RoutineCheckupPOJO(e, elderId)).toList();
+      routineCheckupColl.insertMany(routineCheckupPOJOS);
+      routineCheckupColl.createIndex(Indexes.hashed("elderId"));
+      elderColl.createIndex(Indexes.hashed("{diagnostics.medicationPrescriptions"
+          + ".administrationIds:1}"));
     }
+     */
+
   }
 }
