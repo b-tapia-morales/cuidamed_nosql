8d62e298be38e3044da4851cb8d42c7fa3837b53
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-19 21:12:34 -0400

    Removed unnecessary throws from methods

--- src/main/java/com/bairontapia/projects/cuidamed/daotemplate/IReadAndWriteDAO.java
+++ src/main/java/com/bairontapia/projects/cuidamed/daotemplate/IReadAndWriteDAO.java
@@ -1,10 +1,7 @@
 package com.bairontapia.projects.cuidamed.daotemplate;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 public interface IReadAndWriteDAO<T, ID> extends IReadOnlyDAO<T, ID> {
 
-  void save(T t) throws IOException, SQLException;
+  void save(T t);
 
 }
