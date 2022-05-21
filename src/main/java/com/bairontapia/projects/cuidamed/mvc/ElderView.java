22adf2f626c2b9cf10e9d96dd07db489d8f3601d
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-21 19:24:33 -0400

    Updating fields now fully working

--- src/main/java/com/bairontapia/projects/cuidamed/mvc/ElderView.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/ElderView.java
@@ -3,11 +3,11 @@ package com.bairontapia.projects.cuidamed.mvc;
 import com.bairontapia.projects.cuidamed.mappings.bloodtype.BloodType;
 import com.bairontapia.projects.cuidamed.mappings.gender.Gender;
 import com.bairontapia.projects.cuidamed.mappings.healthcaresystem.HealthCare;
-import com.bairontapia.projects.cuidamed.person.elder.Elder;
 import com.bairontapia.projects.cuidamed.pojo.ElderPOJO;
+import com.bairontapia.projects.cuidamed.pojo.MedicalRecordPOJO;
+import com.bairontapia.projects.cuidamed.pojo.ResponsiblePOJO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
@@ -15,9 +15,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 public class ElderView {
 
@@ -78,22 +76,52 @@ public class ElderView {
   @FXML
   private RoutineCheckupView routineCheckupView;
 
+  public void initialize() {
+    genderComboBox.getItems().addAll(Gender.getValues());
+    bloodTypeComboBox.getItems().addAll(BloodType.getValues());
+    healthCareComboBox.getItems().addAll(HealthCare.getValues());
+    responsibleGenderComboBox.getItems().addAll(Gender.getValues());
+  }
+
+  @FXML
+  public void addColumn(ActionEvent event) {
+  }
+
   public void receiveData(ElderPOJO elder) {
     fillFields(elder);
   }
 
-  @FXML
-  public void addColumn(ActionEvent actionEvent) {
+  private void fillFields(ElderPOJO elder) {
+    fillElderFields(elder);
+    fillMedicalRecordFields(elder.getMedicalRecord());
+    fillResponsibleFields(elder.getResponsible());
   }
 
-  private void fillFields(ElderPOJO elder) {
+  private void fillElderFields(ElderPOJO elder) {
     rut.setText(elder.getRut());
     name.setText(elder.getFirstName());
     lastName.setText(elder.getLastName());
     secondLastName.setText(elder.getSecondLastName());
-    age.setText(elder.getAge().toString());
     birthDatePicker.setValue(elder.getBirthDate());
+    age.setText(elder.getAge().toString());
+    genderComboBox.setValue(Gender.getValueFromName(elder.getGender()));
     isActiveCheckBox.setSelected(elder.getIsActive());
     admissionDatePicker.setValue(elder.getAdmissionDate());
+  }
+
+  private void fillMedicalRecordFields(MedicalRecordPOJO medicalRecord) {
+    bloodTypeComboBox.setValue(BloodType.getValueFromName(medicalRecord.getBloodType()));
+    healthCareComboBox.setValue(HealthCare.getValueFromName(medicalRecord.getHealthCare()));
+  }
+
+  private void fillResponsibleFields(ResponsiblePOJO responsible) {
+    responsibleRut.setText(responsible.getRut());
+    responsibleName.setText(responsible.getFirstName());
+    responsibleLastName.setText(responsible.getLastName());
+    responsibleSecondLastName.setText(responsible.getSecondLastName());
+    responsibleBirthDatePicker.setValue(responsible.getBirthDate());
+    responsibleAge.setText(responsible.getAge().toString());
+    responsibleGenderComboBox.setValue(Gender.getValueFromName(responsible.getGender()));
+    responsibleMobilePhone.setText(responsible.getMobilePhone().toString());
   }
 }
