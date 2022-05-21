5b106e1900681f7b74e222f65a50f5aafb0be563
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-21 15:02:56 -0400

    Created and associated new tabs of tab pane

---
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/MedicationPrescriptionTabPane.java
@@ -0,0 +1,24 @@
+package com.bairontapia.projects.cuidamed.mvc;
+
+import com.bairontapia.projects.cuidamed.pojo.DiagnosticPOJO;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MedicationPrescriptionTabPane {
+
+  @FXML
+  private TableView<DiagnosticPOJO> administrationTable;
+  @FXML
+  private TableColumn<DiagnosticPOJO, String> diseaseName;
+  @FXML
+  private TableColumn<DiagnosticPOJO, LocalDate> diagnosticDate;
+  @FXML
+  private TableColumn<DiagnosticPOJO, String> medicationName;
+  @FXML
+  private TableColumn<DiagnosticPOJO, LocalDateTime> intakeDateTime;
+  @FXML
+  private TableColumn<DiagnosticPOJO, String> intakeStatus;
+}
---
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/RoutineCheckupTabPane.java
@@ -0,0 +1,29 @@
+package com.bairontapia.projects.cuidamed.mvc;
+
+import com.bairontapia.projects.cuidamed.medicalrecord.routinecheckup.RoutineCheckup;
+import java.time.LocalDate;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class RoutineCheckupTabPane {
+
+  @FXML
+  private TableView<RoutineCheckup> checkupTableView;
+  @FXML
+  private TableColumn<RoutineCheckup, LocalDate> checkupDate;
+  @FXML
+  private TableColumn<RoutineCheckup, Double> height;
+  @FXML
+  private TableColumn<RoutineCheckup, Double> weight;
+  @FXML
+  private TableColumn<RoutineCheckup, Double> bmi;
+  @FXML
+  private TableColumn<RoutineCheckup, Integer> heartRate;
+  @FXML
+  private TableColumn<RoutineCheckup, Integer> diastolicPressure;
+  @FXML
+  private TableColumn<RoutineCheckup, Integer> systolicPressure;
+  @FXML
+  private TableColumn<RoutineCheckup, Integer> bodyTemperature;
+}
