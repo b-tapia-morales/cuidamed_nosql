2d63807e3206c537a3febccadbf1fb5e76cc27d0
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-22 14:32:55 -0400

    Modularized Migration class to also reflect latest changes

--- src/main/java/com/bairontapia/projects/cuidamed/migrations/Migration.java
+++ src/main/java/com/bairontapia/projects/cuidamed/migrations/Migration.java
@@ -11,12 +11,18 @@ import com.bairontapia.projects.cuidamed.medicalrecord.routinecheckup.RoutineChe
 import com.bairontapia.projects.cuidamed.person.elder.ElderDAO;
 import com.bairontapia.projects.cuidamed.person.responsible.ResponsibleDAO;
 import com.bairontapia.projects.cuidamed.pojo.DiagnosticPOJO;
+import com.bairontapia.projects.cuidamed.pojo.DiseasePOJO;
+import com.bairontapia.projects.cuidamed.pojo.DiseasePojoDAO;
 import com.bairontapia.projects.cuidamed.pojo.ElderPOJO;
 import com.bairontapia.projects.cuidamed.pojo.MedicalRecordPOJO;
 import com.bairontapia.projects.cuidamed.pojo.MedicationAdministrationPOJO;
+import com.bairontapia.projects.cuidamed.pojo.MedicationPOJO;
+import com.bairontapia.projects.cuidamed.pojo.MedicationPojoDAO;
 import com.bairontapia.projects.cuidamed.pojo.MedicationPrescriptionPOJO;
 import com.bairontapia.projects.cuidamed.pojo.ResponsiblePOJO;
 import com.bairontapia.projects.cuidamed.pojo.RoutineCheckupPOJO;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Indexes;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -26,7 +32,32 @@ import org.bson.types.ObjectId;
 
 public class Migration {
 
+  private static final MongoClient MONGO_CLIENT;
+  private static final MongoDatabase DATABASE;
+
+  static {
+    MONGO_CLIENT = MongoClientSingleton.getInstance();
+    DATABASE = MONGO_CLIENT.getDatabase("admin");
+  }
+
   private Migration() {
+  }
+
+  private static void createDiseaseCollection() throws SQLException, IOException {
+    var diseaseColl = DATABASE.getCollection("disease", DiseasePOJO.class);
+    diseaseColl.drop();
+    var diseases = DiseaseDAO.getInstance().findAll().stream().map(DiseasePOJO::new).toList();
+    diseaseColl.insertMany(diseases);
+    diseaseColl.createIndex(Indexes.hashed("name"));
+  }
+
+  private static void createMedicationCollection() throws SQLException, IOException {
+    var medicationColl = DATABASE.getCollection("medication", MedicationPOJO.class);
+    medicationColl.drop();
+    var medications = MedicationDAO.getInstance().findAll().stream().map(MedicationPOJO::new)
+        .toList();
+    medicationColl.insertMany(medications);
+    medicationColl.createIndex(Indexes.hashed("name"));
   }
 
   private static List<DiagnosticPOJO> retrieveDiagnostics(final String rut)
@@ -34,16 +65,16 @@ public class Migration {
     var diagnostics = DiagnosticDAO.getInstance().findAll(rut);
     var diagnosticPOJOS = new ArrayList<DiagnosticPOJO>();
     for (var diagnostic : diagnostics) {
-      var disease = DiseaseDAO.getInstance().find(diagnostic.diseaseName()).orElseThrow();
+      var disease = DiseasePojoDAO.getInstance().find(diagnostic.diseaseName()).orElseThrow();
       var prescriptions =
-          MedicationPrescriptionDAO.getInstance().findByRutAndDiseaseName(rut, disease.name());
+          MedicationPrescriptionDAO.getInstance().findByRutAndDiseaseName(rut, disease.getName());
       var prescriptionPOJOS = new ArrayList<MedicationPrescriptionPOJO>();
       for (var prescription : prescriptions) {
         var medication =
-            MedicationDAO.getInstance().find(prescription.medicationName()).orElseThrow();
+            MedicationPojoDAO.getInstance().find(prescription.medicationName()).orElseThrow();
         var administrations =
             MedicationAdministrationDAO.getInstance()
-                .findByRutAndMedicationName(rut, medication.name());
+                .findByRutAndMedicationName(rut, medication.getName());
         var administrationPOJOS = administrations.stream()
             .map(MedicationAdministrationPOJO::new).toList();
         prescriptionPOJOS.add(
@@ -57,13 +88,12 @@ public class Migration {
   public static void performMigration() throws SQLException, IOException {
     var client = MongoClientSingleton.getInstance();
     var database = client.getDatabase("admin");
+    createDiseaseCollection();
+    createMedicationCollection();
     var elderColl = database.getCollection("elder", ElderPOJO.class);
     elderColl.drop();
     var routineCheckupColl = database.getCollection("routine_checkup", RoutineCheckupPOJO.class);
     routineCheckupColl.drop();
-    var administrationColl = database.getCollection("medication_administration",
-        MedicationAdministrationPOJO.class);
-    administrationColl.drop();
     for (var elder : ElderDAO.getInstance().findAll()) {
       var elderId = new ObjectId();
       var diagnosticPOJOS = retrieveDiagnostics(elder.rut());
