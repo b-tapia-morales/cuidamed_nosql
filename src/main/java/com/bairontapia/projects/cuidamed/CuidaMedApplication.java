8a91697d4164af8694dfe1fdf3a4764e578a9482
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-24 01:53:08 -0400

    Removed exception throwing from main and replaced it with lombok annotation

--- src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
+++ src/main/java/com/bairontapia/projects/cuidamed/CuidaMedApplication.java
@@ -2,9 +2,11 @@ package com.bairontapia.projects.cuidamed;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import lombok.SneakyThrows;
 
 public class CuidaMedApplication {
 
+  @SneakyThrows
   public static void main(String... args) {
     var mongoLogger = Logger.getLogger("org.mongodb.driver");
     mongoLogger.setLevel(Level.WARNING);
