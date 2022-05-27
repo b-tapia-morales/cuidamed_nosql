ccf249a826646b898057f64aa3ab929fee7ea232
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-27 11:05:41 -0400

    Added icons to stage

--- src/main/java/com/bairontapia/projects/cuidamed/JavaFXApplication.java
+++ src/main/java/com/bairontapia/projects/cuidamed/JavaFXApplication.java
@@ -1,15 +1,18 @@
 package com.bairontapia.projects.cuidamed;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.apache.commons.lang3.StringUtils;
 
 public class JavaFXApplication extends Application {
-
+  
   public static void main(final String... args) {
     launch();
   }
@@ -21,7 +24,16 @@ public class JavaFXApplication extends Application {
     final var scene = new Scene(root);
     stage.setTitle("CuidaMed");
     stage.setScene(scene);
+    stage.getIcons().addAll(generateIcons());
     stage.setResizable(false);
     stage.show();
   }
+
+  private static List<Image> generateIcons() {
+    var sizes = List.of(16, 24, 32, 64);
+    var fileNames = sizes.stream()
+        .map(e -> StringUtils.join("/icons/nursing-", e.toString(), ".png")).toList();
+    return fileNames.stream().map(Image::new).toList();
+  }
+
 }
