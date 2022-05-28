89c1bf0e3c3b1c531a523d007046092740464ae2
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-28 00:48:12 -0400

    Added charts to pane

--- src/main/java/com/bairontapia/projects/cuidamed/mvc/ChartView.java
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/ChartView.java
@@ -2,15 +2,18 @@ package com.bairontapia.projects.cuidamed.mvc;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.ComboBox;
-import javafx.scene.layout.AnchorPane;
 
 public class ChartView {
 
   @FXML
   private ComboBox<String> chartComboBox;
   @FXML
-  private AnchorPane chartPane;
+  private BarChart<String, Long> barChart;
+  @FXML
+  private PieChart pieChart;
 
   public void onChartSelection(ActionEvent actionEvent) {
   }
