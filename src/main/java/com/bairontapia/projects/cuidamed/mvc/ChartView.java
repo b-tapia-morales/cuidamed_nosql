f6a3c76f83c8b4d3053a0d449718262346b85f88
bairon-tapia <b-tapia-morales@users.noreply.github.com>
2022-05-27 21:51:14 -0400

    Added charts view

---
+++ src/main/java/com/bairontapia/projects/cuidamed/mvc/ChartView.java
@@ -0,0 +1,17 @@
+package com.bairontapia.projects.cuidamed.mvc;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+
+public class ChartView {
+
+  @FXML
+  private ComboBox<String> chartComboBox;
+  @FXML
+  private AnchorPane chartPane;
+
+  public void onChartSelection(ActionEvent actionEvent) {
+  }
+}
